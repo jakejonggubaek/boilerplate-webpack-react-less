059088fe1b28e30ef47fc7ee3a99fba968f1b194
jakejonggubaek <jakejonggubaek@gmail.com>
2021-04-20 19:05:45 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
+import "./style/main.less";
+
+const appElement = document.getElementById('root');
+
+ReactDOM.render(<App />, appElement);
